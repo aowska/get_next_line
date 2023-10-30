ecd7afd7da06615db48b7615e91839e8bb7602c1
Alicja Wacowska <awacowsk@c0c7c1.42berlin.de>
2023-10-30 18:45:54 +0100

    iniial

--- get_next_line.h
+++ get_next_line.h
@@ -9,5 +9,23 @@
 /*   Updated: 2023/08/02 21:09:54 by awacowsk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
 
+# include <stdarg.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
 
+# define BUFFER_SIZE 100
+
+char	*get_next_line(int fd);
+char	*ft_strchr(const char *string, int searchedChar );
+void	ft_bzero(void *s, size_t n);
+void	*ft_calloc(size_t elementCount, size_t elementSize);
+size_t	ft_strlen(const char *theString);
+char	*ft_strcat(char *dest, char const *src);
+char	*ft_strcpy(char *dest, char const *src);
+char	*ft_strjoin(char const *s1, char const *s2);
+
+#endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -9,5 +9,103 @@
 /*   Updated: 2023/08/02 21:10:03 by awacowsk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "get_next_line.h"
 
+
+void	*ft_bzero(void *str, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		((unsigned char *)str)[i] = '\0';
+		i++;
+	}
+	return (str);
+}
+
+void	*ft_calloc(size_t number, size_t size)
+{
+	void	*n;
+
+	n = malloc(number * size);
+	if (n != NULL)
+		ft_memset(n, 0, number * size);
+	return (n);
+}
+/* number - liczba argumentow
+size rozmiar kazdego armunemtu */
+char	*ft_strchr(const char *str, int c)
+{
+	c = (unsigned char)c;
+	while ((*str != '\0') && (*str != c))
+		str++;
+	if (*str == c)
+		return ((char *)str);
+	return ((char *) NULL);
+}
+
+size_t	ft_strlen(const char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strcat(char *dest, char const *src)
+{
+	int	a;
+	int	i;
+
+	i = 0;
+	a = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[a] != '\0')
+	{
+		dest[i] = src[a];
+		a++;
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+char	*ft_strcpy(char *dest, char const *src)
+{
+	int	i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*ptr;
+
+	ptr = (char *)malloc (ft_strlen(s1) + ft_strlen(s2) + 1 * sizeof(char));
+	if (ptr == NULL)
+		return (NULL);
+	if (s1 && s2)
+		return (ft_strcat(ft_strcpy(ptr, s1), s2));
+	else if (s1 != NULL && s2 == NULL)
+		return (ft_strcpy(ptr, s1));
+	else if (s1 == NULL && s2 != NULL)
+		return (ft_strcpy(ptr, s2));
+	else
+		return (NULL);
+	return (ptr);
+}
 
